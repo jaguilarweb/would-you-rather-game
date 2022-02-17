0c04a4b2e78c7665a6363a11def3f854348a9de4
Jenny Aguilar <contact@jaguilarweb.com>
2022-02-17 17:23:24 +0100

    feat: Add Nav Component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import sara from '../images/sarah.png';
+import '../index.css';
+import Nav from './Nav'
+
+
+function App() {
+  return (
+    <div >
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
